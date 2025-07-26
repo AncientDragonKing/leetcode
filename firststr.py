2c42a4eeb6d55a4c97346cf649d08a5a0814d419
Nereus Chen <nereuschen0122@gmail.com>
2025-07-26 20:41:32 +0800

    No.28

---
+++ firststr.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        print(haystack.find(needle))
+do = Solution()
+result = do.strStr('leetcode', 'o')
