d1040f9b43197b35941bfbeca3736f92ce5678c8
Nereus Chen <nereuschen0122@gmail.com>
2025-08-17 21:16:18 +0800

    No.43

---
+++ multiply_strings.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        return int(num1) * int(num2)
+solution = Solution()
+print(solution.multiply('123', '456'))
